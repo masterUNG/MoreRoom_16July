d2735f52e7baea3c617e168cbebb0b63ff1e780f
MasterUNG <phrombutr@gmail.com>
2017-07-16 10:02:06 +0700

    Back Controller OK

--- app/src/main/java/app/sutthinant/nant/moreroom/MainActivity.java
+++ app/src/main/java/app/sutthinant/nant/moreroom/MainActivity.java
@@ -2,6 +2,8 @@ package app.sutthinant.nant.moreroom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Back Controller
+        backController();
+
+
     }   //Main Method
+
+    private void backController() {
+        ImageView imageView = (ImageView) findViewById(R.id.imvBack);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
 
 
 }   //Main Class
